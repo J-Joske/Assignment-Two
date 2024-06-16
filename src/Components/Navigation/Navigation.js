c7ff36f5371344b236786a5180c3ec8634f2c5d6
Jarrod Joske <jarrod.joske@gmail.com>
2024-06-16 13:23:03 +1000

    Add images - Add navigation bar menu list

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -2,6 +2,42 @@ import React from "react";
 
 export default function Navigation() {
     return(
-        <div className="navigation"></div>
+        // Navigation bar split into three divs for ease of styling
+        <div className="Navigation">
+            <div className="navigationImage">
+                <img src="" alt="A Richmond shield logo"></img>
+            </div>
+
+            <div className="navigationList">
+                <ul>
+                    <li>
+                        <a href="/AboutUs">About Us</a>
+                        </li>
+                    <li>
+                        <a href="/BehindTheScenes">Behind The Scenes</a>
+                        </li>
+                    <li>
+                        <a href="/GameStats">Game Stats</a>
+                        </li>
+                    <li>
+                        <a href="/PlayerProfiles">Player Profiles</a>
+                        </li>
+                    <li>
+                        <a href="/Socials">Socials</a>
+                        </li>
+                    <li>
+                        <a href="/Membership">Membership</a>
+                        </li>
+                    <li>
+                        <a href="/ContactUs">Contact Us</a>
+                        </li>
+                </ul>
+            </div>
+
+            <div className="searchBar"> 
+
+            </div>
+
+        </div>
     );
 }
