e7653bcc42a8e4b8a2a80884e45d027988ddd03e
Jarrod Joske <jarrod.joske@gmail.com>
2024-06-18 10:06:13 +1000

    About Us componant added in content / images. Added styling to About Us and overall heading styling.

--- src/Components/Main/AboutUs.js
+++ src/Components/Main/AboutUs.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import '../../Styles/AboutUs_Styles.css';
+import VflTeam from '../Images/vfl-team.png';
+import TeamWin from '../Images/win-image.avif';
+import TeamCelebrate from '../Images/celebrate.avif';
 
 const AboutUs = () => {
     return (
@@ -7,20 +11,23 @@ const AboutUs = () => {
             <h2>ABOUT US</h2>
 
             <div className="AboutUsArticle">
+                <img className="aboutUsImages" src={VflTeam} alt="Richmond VFL Team"></img>
                 <h3>HISTORY</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec orci nulla. In a mollis nunc. Fusce nec eros lobortis, dapibus ligula vel, dapibus ex. Curabitur diam mi, faucibus eget urna eu, feugiat tincidunt erat. Aliquam eu sodales purus, ac feugiat nisl. Quisque auctor consectetur tempor. Sed facilisis mi nisl. Sed tempus volutpat lacinia. Suspendisse egesta</p>
-
-            </div>
-
-            <div className="AboutUsArtcile">
-                <h3>WHO WE ARE</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec orci nulla. In a mollis nunc. Fusce nec eros lobortis, dapibus ligula vel, dapibus ex. Curabitur diam mi, faucibus eget urna eu, feugiat tincidunt erat. Aliquam eu sodales purus, ac feugiat nisl. Quisque auctor consectetur tempor. Sed facilisis mi nisl. Sed tempus volutpat lacinia. Suspendisse egesta</p>
+                <p className="aboutUsContent">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec orci nulla. In a mollis nunc. Fusce nec eros lobortis, dapibus ligula vel, dapibus ex. Curabitur diam mi, faucibus eget urna eu, feugiat tincidunt erat. Aliquam eu sodales purus, ac feugiat nisl. Quisque auctor consectetur tempor. Sed facilisis mi nisl. Sed tempus volutpat lacinia. Suspendisse egesta</p>
 
             </div>
 
             <div className="AboutUsArticle">
+                <img className="aboutUsImages" src={TeamWin} alt="Team Winning"></img>
+                <h3>WHO WE ARE</h3>
+                <p className="aboutUsContent">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec orci nulla. In a mollis nunc. Fusce nec eros lobortis, dapibus ligula vel, dapibus ex. Curabitur diam mi, faucibus eget urna eu, feugiat tincidunt erat. Aliquam eu sodales purus, ac feugiat nisl. Quisque auctor consectetur tempor. Sed facilisis mi nisl. Sed tempus volutpat lacinia. Suspendisse egesta</p>
+
+            </div>
+
+            <div className="AboutUsArticle">
+                <img className="aboutUsImages" src={TeamCelebrate} alt="Team celebrating"></img>
                 <h3>RICHMOND</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec orci nulla. In a mollis nunc. Fusce nec eros lobortis, dapibus ligula vel, dapibus ex. Curabitur diam mi, faucibus eget urna eu, feugiat tincidunt erat. Aliquam eu sodales purus, ac feugiat nisl. Quisque auctor consectetur tempor. Sed facilisis mi nisl. Sed tempus volutpat lacinia. Suspendisse egesta</p>
+                <p className="aboutUsContent">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec orci nulla. In a mollis nunc. Fusce nec eros lobortis, dapibus ligula vel, dapibus ex. Curabitur diam mi, faucibus eget urna eu, feugiat tincidunt erat. Aliquam eu sodales purus, ac feugiat nisl. Quisque auctor consectetur tempor. Sed facilisis mi nisl. Sed tempus volutpat lacinia. Suspendisse egesta</p>
 
             </div>
 
