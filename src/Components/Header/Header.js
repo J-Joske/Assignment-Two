562b03d43759c4f0e9795656cfb251c6ea875e01
Jarrod Joske <jarrod.joske@gmail.com>
2024-06-18 11:22:36 +1000

    Added Header images and styling

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,15 @@
 import React from "react";
+import '../../Styles/Header_Styles.css';
 import RichmondHeader from "../Images/richmond-header.png";
+import RichmondLogo from "../Images/logo-Richmond-Tigers.png";
 
 console.log(RichmondHeader);
 
 export default function Header() {
     return (
         <div className="Header">
-            <div className="richmondHeader">
-            <img src={RichmondHeader} alt="Richmond with claw marks"></img>
-            </div>
+            <img className="richmondHeaderLogo" src={RichmondHeader} alt="Richmond with claw marks"></img>
+            <img className="richmondLogo" src={RichmondLogo} alt="richmond logo"></img>
         </div>
     );
 }
