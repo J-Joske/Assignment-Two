17ea9f9c1aa7160c31685e04234744fb91df923b
Jarrod Joske <jarrod.joske@gmail.com>
2024-06-21 22:56:03 +1000

    implemented nav bar hide on scoll

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,4 +1,5 @@
-import React from "react";
+// import React from "react";
+import React, { useState, useEffect } from 'react';
 import HomeOfRichmond from "../Images/home_of_richmond.png";
 import "../../Styles/Navigation_Styles.css";
 import {NavigationItems} from '../Footer/Footer.js';
@@ -6,10 +7,46 @@ import {NavigationItems} from '../Footer/Footer.js';
 console.log(HomeOfRichmond);
 
 
-export default function Navigation() {
+// hide menubar on scroll
+
+    const ScrollNavbar = () => {
+     
+      const [prevScrollpos, setPrevScrollpos] = useState(window.pageYOffset);
+      const [top, setTop] = useState(0);
+      useEffect(() => {
+        
+        const handleScroll = () => {
+          const currentScrollPos = window.pageYOffset;
+          if (prevScrollpos > currentScrollPos) {
+            setTop(0); 
+          } else {
+            setTop(-50);
+          }
+          setPrevScrollpos(currentScrollPos);
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+        
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, [prevScrollpos]);
+
+      // Styles for the navbar
+      const navbarStyle = {
+        backgroundColor: 'black',
+        position: 'fixed',
+        top: `${top}px`,
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        transition: 'top 0.3s',
+      };
+    
     return(
         // Navigation bar split into three divs for ease of styling
-        <div className="Navigation">
+        <div id="navbar" style={navbarStyle}>
             
                 <img className="navigationImage" src={HomeOfRichmond} alt="A Richmond shield logo"></img>
 
@@ -29,6 +66,8 @@ export default function Navigation() {
 
             </div>
 
-        </div>
+                </div> 
+        
     );
-}
+} 
+export default ScrollNavbar;
